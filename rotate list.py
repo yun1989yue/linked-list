d6e9dc1d97b9b835bf99732b7529a6224507ea05
yun1989yue <418292922@qq.com>
2015-11-06 23:16:02 -0800

    Create rotate list.py

---
+++ rotate list.py
@@ -0,0 +1,33 @@
+'''
+Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+'''
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} k
+    # @return {ListNode}
+    def rotateRight(self, head, k): # notice that k may be larger than lenght of list
+        if not head or not head.next:
+            return head
+        temp = head
+        l = 0
+        while temp:
+            l += 1
+            temp = temp.next
+        k %= l
+        if k == 0:
+            return head
+        end = head
+        for i in xrange(k):
+            end = end.next
+        start = head
+        while end.next:
+            end = end.next
+            start = start.next
+        Head = start.next
+        start.next = None
+        end.next = head
+        return Head
