77daf1225ab2faffccb7db0f174725c10ef0fffa
yun1989yue <418292922@qq.com>
2015-11-08 23:59:24 -0800

    Update partition list.py

--- partition list.py
+++ partition list.py
@@ -8,6 +8,28 @@ Given 1->4->3->2->5->2 and x = 3,
 return 1->2->2->4->3->5.
 '''
 class Solution():
+    def partition(self, head, x): # notice that the title requests preservation of original list
+        small = ListNode(x-1)
+        ts = small
+        large = ListNode(x)
+        tl = large
+        temp = head
+        while temp:
+            if temp.val < x:
+                ts.next = temp
+                ts = ts.next
+            else:
+                tl.next = temp
+                tl = tl.next
+            temp = temp.next
+        ts.next = large.next
+        tl.next = None
+        return small.next
+
+'''
+In-place Method
+'''
+class Solution():
     def partition(self, head, x):
         tempHead = ListNode(x-1)
         tempHead.next = head
