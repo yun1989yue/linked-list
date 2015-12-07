9b33a6aee4f3c4b6ef8059579eb3bf94c2e5c993
yun1989yue <418292922@qq.com>
2015-12-07 13:04:44 -0800

    Create palindrome linked list.py

---
+++ palindrome linked list.py
@@ -0,0 +1,62 @@
+'''
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+'''
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return True
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        current = slow.next
+        slow.next = None
+        pre = None
+        while current:
+            temp = current
+            current = current.next
+            temp.next = pre
+            pre = temp
+        temp = head
+        while pre:
+            if pre.val != temp.val:
+                return False
+            pre = pre.next
+            temp = temp.next
+        return True
+        
+'''
+if O(n) space used, we can use stack
+'''
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return True
+        stack = [head.val]
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            stack.append(slow.val)
+            fast = fast.next.next
+        if not fast:
+            stack.pop()
+        temp = slow.next
+        while temp:
+            key = stack.pop()
+            if key != temp.val:
+                return False
+            temp = temp.next
+        return True
